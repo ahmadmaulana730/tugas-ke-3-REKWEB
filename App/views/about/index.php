1e782a45ed082c174f8a0cbc66f82f4c309fea3a
Ahmad maulana <amkomarudin7@gmail.com>
2019-10-02 20:55:04 +0700

    MVC dengan PHP#6 Assets

--- App/views/about/index.php
+++ App/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About me</h1>
-<p>Hallo Nama saya <?= $data['nama']; ?>,umur saya <?= $data['umur']; ?>tahun,saya adalah seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+    <h1>About me</h1>
+    <img src="<?= BASEURL ?>/img/pp.JPEG" alt="" width="200" class="rounded-circle shadow">
+    <p>Hallo Nama saya <?= $data['nama']; ?>,umur saya <?= $data['umur']; ?>tahun,saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
