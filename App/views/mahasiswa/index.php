09f4078b43c8ce7ed1493e3680679b0833bbfbb5
Ahmad maulana <amkomarudin7@gmail.com>
2019-10-03 16:17:23 +0700

    MVC dengan PHP#11 Delete

--- App/views/mahasiswa/index.php
+++ App/views/mahasiswa/index.php
@@ -19,11 +19,12 @@
 
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <li class="list-group-item  align-items-center">
 
                         <?= $mhs['nama']; ?>
 
-                        <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary">Detail</a>
+                        <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right ml-2" onclick="return confirm('yakin');">Hapus</a>
+                        <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right ml-2">Detail</a>
                     </li>
 
                 <?php endforeach; ?>
